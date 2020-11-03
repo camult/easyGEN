14ab129bbea9e6b2c0e3cf57b4cb93e0fe34d59f
Fernando Brito Lopes <camult@gmail.com>
2020-11-03 11:06:51 -0600

    Add files via upload

--- ezGEN_Create.R
+++ ezGEN_Create.R
@@ -3,32 +3,21 @@ library("Rcpp")
 library("devtools")
 library("roxygen2")
 
-setwd("C:\\Users\\Camult\\Documents\\GitHub\\easyGEN")
+setwd("C:\\Users\\BRITOLOPESF\\Documents\\GitHub\\easyGEN")
 roxygenise()
 
 
 library("Rd2md")
-setwd("C:\\Users\\Camult\\Documents\\GitHub\\ezCV")
-Rd2markdown(rdfile="C:\\Users\\Camult\\Documents\\GitHub\\ezCV\\man\\GBLUP_CV.Rd",
-            outfile="C:\\Users\\Camult\\Documents\\GitHub\\ezCV\\README.md")
-Rd2markdown(rdfile="C:\\Users\\Camult\\Documents\\GitHub\\ezCV\\man\\cvBGBLUP.Rd",
-            outfile="C:\\Users\\Camult\\Documents\\GitHub\\ezCV\\README.md", append=TRUE)
-Rd2markdown(rdfile="C:\\Users\\Camult\\Documents\\GitHub\\ezCV\\man\\RRBLUP_CV.Rd",
-            outfile="C:\\Users\\Camult\\Documents\\GitHub\\ezCV\\README.md", append=TRUE)
-Rd2markdown(rdfile="C:\\Users\\Camult\\Documents\\GitHub\\ezCV\\man\\cvBayes.Rd",
-            outfile="C:\\Users\\Camult\\Documents\\GitHub\\ezCV\\README.md", append=TRUE)
-Rd2markdown(rdfile="C:\\Users\\Camult\\Documents\\GitHub\\ezCV\\man\\MME.Rd",
-            outfile="C:\\Users\\Camult\\Documents\\GitHub\\ezCV\\README.md", append=TRUE)
-Rd2markdown(rdfile="C:\\Users\\Camult\\Documents\\GitHub\\ezCV\\man\\LOOCV_DG.Rd",
-            outfile="C:\\Users\\Camult\\Documents\\GitHub\\ezCV\\README.md", append=TRUE)
-Rd2markdown(rdfile="C:\\Users\\Camult\\Documents\\GitHub\\ezCV\\man\\kCV_DG.Rd",
-            outfile="C:\\Users\\Camult\\Documents\\GitHub\\ezCV\\README.md", append=TRUE)
-Rd2markdown(rdfile="C:\\Users\\Camult\\Documents\\GitHub\\ezCV\\man\\RRBLUP.Rd",
-            outfile="C:\\Users\\Camult\\Documents\\GitHub\\ezCV\\README.md", append=TRUE)
-Rd2markdown(rdfile="C:\\Users\\Camult\\Documents\\GitHub\\ezCV\\man\\LOOrrDG.Rd",
-            outfile="C:\\Users\\Camult\\Documents\\GitHub\\ezCV\\README.md", append=TRUE)
+Rd2markdown(rdfile= "C:\\Users\\BRITOLOPESF\\Documents\\GitHub\\easyGEN\\man\\remlf90.Rd",
+            outfile="C:\\Users\\BRITOLOPESF\\Documents\\GitHub\\easyGEN\\README.md")
+Rd2markdown(rdfile= "C:\\Users\\BRITOLOPESF\\Documents\\GitHub\\easyGEN\\man\\gibbsf90.Rd",
+            outfile="C:\\Users\\BRITOLOPESF\\Documents\\GitHub\\easyGEN\\README.md", append=TRUE)
+Rd2markdown(rdfile= "C:\\Users\\BRITOLOPESF\\Documents\\GitHub\\easyGEN\\man\\PostGibbs.Rd",
+            outfile="C:\\Users\\BRITOLOPESF\\Documents\\GitHub\\easyGEN\\README.md", append=TRUE)
+Rd2markdown(rdfile= "C:\\Users\\BRITOLOPESF\\Documents\\GitHub\\easyGEN\\man\\SEMeff.Rd",
+            outfile="C:\\Users\\BRITOLOPESF\\Documents\\GitHub\\easyGEN\\README.md", append=TRUE)
 
-setwd("C:\\Users\\Camult\\Documents\\GitHub")
-system("R CMD check ezCV")
-system("R CMD build ezCV")
+setwd("C:\\Users\\BRITOLOPESF\\Documents\\GitHub")
+system("R CMD check easyGEN")
+system("R CMD build easyGEN")
 system("R CMD INSTALL ezCV_1.0.tar.gz")
